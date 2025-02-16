7994ee8cca2553d97edd75de4f9e03cd78cdcd89
Vaishnav Raut <vaishnav.raut@gmail.com>
2025-02-16 21:13:39 +0530

    Added Data

--- DbOperationWithEF/DbOperationWithEF/Program.cs
+++ DbOperationWithEF/DbOperationWithEF/Program.cs
@@ -1,7 +1,10 @@
+using DbOperationWithEF.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnString")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
